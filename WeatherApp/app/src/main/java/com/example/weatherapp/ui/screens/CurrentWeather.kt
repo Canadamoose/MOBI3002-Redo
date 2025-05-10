ef96a115327f091f777adc13749b4679c70f0537
Canadamoose <canadamoose@live.ca>
2025-05-09 23:54:14 -0300

    Sprint 2

--- WeatherApp/app/src/main/java/com/example/weatherapp/ui/screens/CurrentWeather.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/ui/screens/CurrentWeather.kt
@@ -1,4 +1,42 @@
 package com.example.weatherapp.ui.screens
 
-class CurrentWeather {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.weatherapp.R
+
+@Composable
+fun CurrentWeather() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(state = rememberScrollState()))  {
+
+        // Weather image
+        Image(
+            painter = painterResource(id = R.drawable.weather_icon),
+            contentDescription = "Weather Icon")
+
+        //Temp
+        Text("72°F")
+
+        //Condition
+        Text("Sunny")
+
+        //Precipitation
+        Text("Precipitation: 0%")
+
+        //Wind speed and direction
+        Text("Wind: 5 mph East")
+    }
 }
--- WeatherApp/app/src/main/java/com/example/weatherapp/ui/screens/DailyForecast.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/ui/screens/DailyForecast.kt
@@ -1,4 +1,76 @@
 package com.example.weatherapp.ui.screens
 
-class DailyForecast {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.*
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.weatherapp.R
+
+@Composable
+fun DailyForecast() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 25.dp)
+            .verticalScroll(state = rememberScrollState())) {
+
+        //Date
+        Text("Tomorrow")
+        // Weather image
+        Image(
+            painter = painterResource(id = R.drawable.weather_icon),
+            contentDescription = "Weather Icon"
+        )
+        //Temp
+        Text("72°F")
+        //Condition
+        Text("Sunny")
+        //Precipitation
+        Text("Precipitation: 0%")
+        //Wind speed and direction
+        Text("Wind: 5 mph East")
+
+        //2nd day
+        Spacer(Modifier.height(50.dp))
+        //Date
+        Text("The next Day")
+        // Weather image
+        Image(
+            painter = painterResource(id = R.drawable.weather_icon),
+            contentDescription = "Weather Icon"
+        )
+        //Temp
+        Text("72°F")
+        //Condition
+        Text("Sunny")
+        //Precipitation
+        Text("Precipitation: 0%")
+        //Wind speed and direction
+        Text("Wind: 5 mph East")
+
+        //3rd day
+        Spacer(Modifier.height(50.dp))
+        //Date
+        Text("The other next day")
+        // Weather image
+        Image(
+            painter = painterResource(id = R.drawable.weather_icon),
+            contentDescription = "Weather Icon"
+        )
+        //Temp
+        Text("72°F")
+        //Condition
+        Text("Sunny")
+        //Precipitation
+        Text("Precipitation: 0%")
+        //Wind speed and direction
+        Text("Wind: 5 mph East")
+
+    }
 }
